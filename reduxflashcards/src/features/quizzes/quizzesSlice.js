6e75bd124c260a790bbd05b9b87537f6e1cafbe0
Rhett Muse <rhett.muse123@gmail.com>
2022-07-23 12:31:04 -0500

    add quiz functionality

--- reduxflashcards/src/features/quizzes/quizzesSlice.js
+++ reduxflashcards/src/features/quizzes/quizzesSlice.js
@@ -1,8 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { addQuizIdToTopic } from "../topics/topicsSlice";
 
 const options = {
     name: 'quizzes',
-    intialState: {quizzes: {}},
+    initialState: { quizzes: { 
+        '456': {
+            id: '456',
+            topicId: '123',
+            name: 'quiz for example topic',
+            cardIds: ['789', '101', '102']
+        }
+    }},
     reducers: {
         addQuiz(state, action) {
             return {...state,
@@ -16,12 +24,19 @@ const options = {
                         }
                 }
             }
-        }
+        },
+    }
+}
+
+export const createQuiz = (payload) => {
+    return dispatch => {
+        dispatch(addQuiz(payload));
+        dispatch(addQuizIdToTopic(payload));
     }
 }
 
 export const quizzesSlice = createSlice(options); 
 
-export const selectQuizzes = (state) => state.quizzes.quizzes;
+export const selectQuizzes = state => state.quizzes.quizzes;
 export const { addQuiz } = quizzesSlice.actions; 
 export default quizzesSlice.reducer; 
--- reduxflashcards/src/features/topics/topicsSlice.js
+++ reduxflashcards/src/features/topics/topicsSlice.js
@@ -2,26 +2,37 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const options = {
     name: 'topics',
-    initialState: { topics: {} },
+    initialState: { topics: {
+        '123': {
+            id: '123',
+            name: 'example topic',
+            icon: 'icon url',
+            quizIds: ['456']
+        }
+    }},
     reducers: {
         addTopic(state, action) {
             return {...state, 
                 topics: { 
                     ...state.topics,
                         [action.payload.id]: { 
-                        id: action.payload.id,
-                        name: action.payload.name,
-                        icon: action.payload.icon,
-                        quizIds: []
-                    }
+                            id: action.payload.id,
+                            name: action.payload.name,
+                            icon: action.payload.icon,
+                            quizIds: []
+                        }
                 }
             }
+        },
+        addQuizIdToTopic(state, action) {
+            state.topics[action.payload.topicId].quizIds.push(action.payload.id)
         }
     }
 }
+
 export const topicsSlice = createSlice(options);
 
 export const selectTopics = (state) => state.topics.topics; 
-export const { addTopic } = topicsSlice.actions; 
+export const { addTopic, addQuizIdToTopic } = topicsSlice.actions; 
 export default topicsSlice.reducer;
 
