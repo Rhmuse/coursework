3b2bf6688ba1abb047779326bd41532c5130c575
Rhett Muse <rhett.muse123@gmail.com>
2022-09-20 10:51:26 -0500

    set up quoteAPI route and get quotes method

--- quote-api/server.js
+++ quote-api/server.js
@@ -1,10 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const { quotes } = require("./data");
+const { getRandomElement } = require("./utils");
 
 const PORT = process.env.PORT || 4001;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+const quoteAPI = express.Router();
+
+app.use("/api/quotes", quoteAPI);
+
+quoteAPI.use((req, res, next) => {
+  console.log("Time: ", Date.now());
+  next();
+});
+
+quoteAPI.get("/", (req, res, next) => {
+  res.send(quotes);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port:${PORT}`);
+});
