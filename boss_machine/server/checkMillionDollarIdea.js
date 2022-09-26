2d461e8413eed6df3bad5e766cb8075c3f08f26c
Rhett Muse <rhett.muse123@gmail.com>
2022-09-26 13:43:24 -0500

    wrtite checkMillionDollarIdea.js middleware

--- boss_machine/server/checkMillionDollarIdea.js
+++ boss_machine/server/checkMillionDollarIdea.js
@@ -1,4 +1,14 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) => {
+	const idea = req.body;
+	const totalValue = idea.numWeeks * idea.weeklyRevenue;
+	if (totalValue > 1000000) {
+		next();
+	} else {
+		res.status(400).send();
+		throw new Error('Idea must be worth at least $1,000,000');
+	}
+	return false;
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
--- boss_machine/server/ideas.js
+++ boss_machine/server/ideas.js
@@ -1,21 +1,46 @@
-const express = require("express");
+const express = require('express');
+const checkMillionDollarIdea = require('./checkMillionDollarIdea');
 const ideasRouter = express.Router();
 const {
-  getAllFromDatabase,
-  addToDatabase,
-  getFromDatabaseById,
-  updateInstanceInDatabase,
-  deleteFromDatabasebyId,
-} = require("./db");
+	getAllFromDatabase,
+	addToDatabase,
+	getFromDatabaseById,
+	updateInstanceInDatabase,
+	deleteFromDatabasebyId,
+} = require('./db');
 
-ideasRouter.use("/", (req, res, next) => {
-  console.log("ideasRouter accessed at ", Date.now());
-  next();
+ideasRouter.use('/', (req, res, next) => {
+	console.log('ideasRouter accessed at ', Date.now());
+	next();
 });
 
-ideasRouter.get("/", (req, res, next) => {
-  const allIdeas = getAllFromDatabase("ideas");
-  res.send(allIdeas);
+ideasRouter.get('/', (req, res, next) => {
+	const allIdeas = getAllFromDatabase('ideas');
+	res.send(allIdeas);
+});
+
+ideasRouter.post('/', checkMillionDollarIdea, (req, res, next) => {
+	const newIdea = req.body;
+	addToDatabase('ideas', newIdea);
+	res.send(newIdea);
+});
+
+ideasRouter.get('/:ideaId', (req, res, next) => {
+	const ideaId = req.params.ideaId;
+	const requestedIdea = getFromDatabaseById('ideas', ideaId);
+	res.send(requestedIdea);
+});
+
+ideasRouter.put('/:ideaId', checkMillionDollarIdea, (req, res, next) => {
+	const updatedIdea = req.body;
+	updateInstanceInDatabase('ideas', updatedIdea);
+	res.send(updatedIdea);
+});
+
+ideasRouter.delete('/:ideaId', (req, res, next) => {
+	const ideaId = req.params.ideaId;
+	const deletedIdea = deleteFromDatabasebyId('ideas', ideaId);
+	res.send(deletedIdea);
 });
 
 module.exports = ideasRouter;
