be036cd26912d8f492f5427bd157e25c3ee1d432
Rhett Muse <rhett.muse123@gmail.com>
2022-10-01 09:51:31 -0500

    add routing

--- oauth-project/app.js
+++ oauth-project/app.js
@@ -76,7 +76,7 @@ app.get('/', (req, res) => {
 	res.render('index', { user: req.user });
 });
 
-app.get('/account', (req, res) => {
+app.get('/account', ensureAuthenticated, (req, res) => {
 	res.render('account', { user: req.user });
 });
 
@@ -89,6 +89,21 @@ app.get('/logout', (req, res) => {
 	res.redirect('/');
 });
 
+app.get(
+	'/auth/github',
+	passport.authenticate('github', {
+		scope: ['user'],
+	})
+);
+
+app.get(
+	'/auth/github/callback',
+	passport.authenticate('github', {
+		failureRedirect: '/login',
+		successRedirect: '/',
+	})
+);
+
 /*
  * Listener
  */
@@ -98,3 +113,10 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 /*
  * ensureAuthenticated Callback Function
  */
+
+const ensureAuthenticated = (req, res, next) => {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect('/login');
+};
